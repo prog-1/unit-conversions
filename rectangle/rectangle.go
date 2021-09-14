07e658da9988c0f65bba79cecd40730a2f3f3ca9
Pavel Zaichenkov <zaichenkov@gmail.com>
2021-09-15 01:21:30 +0200

    aaa

--- rectangle/rectangle.go
+++ rectangle/rectangle.go
@@ -13,5 +13,6 @@ Perimeter: 19.00, square: 22.00
 */
 func main() {
 	fmt.Println("The program prints the perimeter and the square of a rectangle given the rectangle sides.")
+	fmt.Println("AAA")
 	// ++ Your code here! ++
 }
