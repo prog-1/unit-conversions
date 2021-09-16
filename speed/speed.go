53879aa7c48267ce1352c30bee91efac3fa3da78
Richard Shashalevvich <richardsasalevic@gmail.com>
2021-09-16 22:49:11 +0300

    modified speed.go

--- speed/speed.go
+++ speed/speed.go
@@ -13,5 +13,8 @@ The speed in m/s: 27.78
 */
 func main() {
 	fmt.Println("The program converts km/h to m/s.")
-	// ++ Your code here! ++
+	fmt.Print("Enter the speed in km/h: ")
+	var kmh float64
+	fmt.Scan(&kmh)
+	fmt.Printf("The speed in m/s: %.2f", kmh/60/60*1000)
 }
