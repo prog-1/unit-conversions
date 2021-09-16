5e5e88f76f5374fb63f5f86cd54584d0b49fbfbe
Jaroslavs Samcuks <yarcat@gmail.com>
2021-09-16 16:30:57 +0200

    rectangle: add implementation

--- rectangle/rectangle.go
+++ rectangle/rectangle.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-/* 
+/*
 
 Sample input/output:
 
@@ -13,5 +13,9 @@ Perimeter: 19.00, square: 22.00
 */
 func main() {
 	fmt.Println("The program prints the perimeter and the square of a rectangle given the rectangle sides.")
+	fmt.Print("Enter the length and the bredth of the rectangle: ")
+	var l, b float64
+	fmt.Scanln(&l, &b)
+	fmt.Printf("Perimter: %.2f, square: %.2f\n", (l+b)*2, l*b)
 	// ++ Your code here! ++
 }
