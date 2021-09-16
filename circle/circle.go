9432d760bbcce91dbfb95f44035b9a48ada98c59
Richard Shashalevvich <richardsasalevic@gmail.com>
2021-09-16 22:44:41 +0300

    modifed circule.go

--- circle/circle.go
+++ circle/circle.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 /*
@@ -15,6 +16,10 @@ Perimeter: 50.27, square: 201.06
 */
 func main() {
 	fmt.Println("The program prints the perimeter and the square of a circle given the radius.")
+	fmt.Print("Enter the radius: ")
+	var r float64
+	fmt.Scan(&r)
+	fmt.Printf("Perimeter: %.2f, square: %.2f", 2*r*math.Pi, math.Pi*r*r)
 	// ++ Your code here! ++
 	//
 	// Hint: Use math.Pi constant (https://pkg.go.dev/math#pkg-constants) to obtain π value.
