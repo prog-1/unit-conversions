82a30f6d2736ef751f2837377c82690efce5e1d8
Richard Shashalevvich <richardsasalevic@gmail.com>
2021-09-16 22:52:39 +0300

    modified temperature.go

--- temperature/temperature.go
+++ temperature/temperature.go
@@ -13,5 +13,8 @@ The temperature in Fahrenheit: 212.00
 */
 func main() {
 	fmt.Println("The program converts temperature from Celsius to Fahrenheit.")
-	// ++ Your code here! ++
+	fmt.Print("Enter the temperature in Celsius: ")
+	var cel float64
+	fmt.Scan(&cel)
+	fmt.Printf("The temperature in Fahrenheit: %.2f", (cel*9/5)+32)
 }
