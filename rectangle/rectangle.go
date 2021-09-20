296eed67b2a4e56e7d8a7d01e2d5dc50d52f2546
Jaroslavs Samcuks <yarcat@gmail.com>
2021-09-20 09:51:06 +0200

    removed space

--- rectangle/rectangle.go
+++ rectangle/rectangle.go
@@ -16,8 +16,8 @@ Square: 22
 func main() {
 	fmt.Println("The program prints the perimeter and the square of a rectangle given the rectangle sides.")
 	var l, w float64
-	fmt.Println("Enter the length and the bredth of the rectangle: ")
-	fmt.Scanln(&l, &w)
+	fmt.Println("Enter the length and the bredth of the rectangle:")
+	fmt.Scan(&l, &w)
 	fmt.Println("Perimeter:", (l+w)*2)
 	fmt.Println("Square:", l*w)
 }
