de79d1ec039ca71c43c33bfccec1385c5a267cd7
Vladimir Satinov <vovaumni@gmail.com>
2021-09-22 19:33:22 +0300

    modified

--- speed/speed.go
+++ speed/speed.go
@@ -15,4 +15,8 @@ The speed in m/s: 27.77777777777778
 func main() {
 	fmt.Println("The program converts km/h to m/s.")
 	// ++ Your code here! ++
+	var s float64
+	fmt.Println("Enter the speed in km/h:")
+	fmt.Scan(&s)
+	fmt.Println("The speed in m/s:", s/3.6)
 }
--- temperature/temperature.go
+++ temperature/temperature.go
@@ -15,4 +15,8 @@ The temperature in Fahrenheit: 212
 func main() {
 	fmt.Println("The program converts temperature from Celsius to Fahrenheit.")
 	// ++ Your code here! ++
+	var T float64
+	fmt.Println("Enter the temperature in Celsius:")
+	fmt.Scan(&T)
+	fmt.Println("The temperature in Fahrenheit:", T*1.8+32)
 }
