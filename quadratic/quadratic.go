04d77c3a64e231cb24e08d2dec840fc1f77774b4
Aleksej Pjavka <karpenadij@gmail.com>
2021-09-25 11:21:10 +0300

    redone :)

--- quadratic/quadratic.go
+++ quadratic/quadratic.go
@@ -11,8 +11,7 @@ func main() {
 	var a, b, c float64
 	fmt.Scan(&a, &b, &c)
 	var x1, x2 float64
-	x1 = (-b - math.Sqrt(b*b-4*a*c)) / 2 * a
-	x2 = (-b + math.Sqrt(b*b-4*a*c)) / 2 * a
+	x1 = (-b - math.Sqrt(b*b-4*a*c)) / (2 * a)
+	x2 = (-b + math.Sqrt(b*b-4*a*c)) / (2 * a)
 	fmt.Println("x1=", x1, "x2=", x2)
-
 }
