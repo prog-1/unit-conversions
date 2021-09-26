df374a2188c44b67b655b994836f05f4fa372cd5
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-09-26 20:53:04 +0300

    changing quadratic.go

--- quadratic/quadratic.go
+++ quadratic/quadratic.go
@@ -6,11 +6,8 @@ import (
 )
 
 /*Given coefficients A, B, and C of a quadratic equation Ax^2 + Bx + C = 0, write a program that prints the equation solutions x1 and x2.
-
 You will need to use math.Sqrt function to calculate a square root.
-
 Example:
-
 The program finds solutions of a quadratic equation.
 Enter the coefficients A, B and C:
 1 3 -10
@@ -19,22 +16,21 @@ x2 = -5 */
 func main() {
 	fmt.Println("The program finds solutions of a quadratic equation.")
 	fmt.Println("Enter the coefficients A, B and C:")
-	var A float64
-	var B float64
-	var C float64
+	var A, B, C float64
 	fmt.Scan(&A, &B, &C)
 	var D float64 = B*B - 4*A*C
-	if D > 0 {
-		var x1 float64 = (-B + math.Sqrt(D)) / (2 * A)
-		var x2 float64 = (-B - math.Sqrt(D)) / (2 * A)
-		fmt.Printf("x1: %v\n", x1)
-		fmt.Printf("x2: %v", x2)
+	if A == 0 {
+		fmt.Println("x = 0")
 	}
-	if D == 0 {
-		var x float64 = -B / (2 * A)
-		fmt.Printf("x: %v", x)
-	}
-	if D < 0 {
-		fmt.Println("No answer.")
+	if A != 0 {
+		if D >= 0 {
+			x1 := (-B + math.Sqrt(D)) / (2 * A)
+			x2 := (-B - math.Sqrt(D)) / (2 * A)
+			fmt.Printf("x1: %v\n", x1)
+			fmt.Printf("x2: %v", x2)
+		}
+		if D < 0 {
+			fmt.Println("No answer.")
+		}
 	}
 }
