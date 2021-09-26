714fadd0f1dc8f64b8b795c0d78e3bd47d6ea930
Денис Максимкин <you@example.com>
2021-09-26 22:41:16 +0300

    adding temperature

--- temperature/temperature.go
+++ temperature/temperature.go
@@ -14,5 +14,10 @@ The temperature in Fahrenheit: 212
 */
 func main() {
 	fmt.Println("The program converts temperature from Celsius to Fahrenheit.")
-	// ++ Your code here! ++
+	var tempC float64
+	fmt.Print("Enter the temperature in Celsius:")
+	fmt.Scan(&tempC)
+	var tempF float64
+	tempF = tempC*9/5 + 32
+	fmt.Println("The temperature in Fahrenheit:", tempF)
 }
