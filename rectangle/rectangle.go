f0eb37b9286455b3f7cc04982ecc1b07a64bca83
Sp0ozy <imolchanov210@gmail.com>
2021-09-26 11:05:42 +0300

    rectangle done

--- rectangle/rectangle.go
+++ rectangle/rectangle.go
@@ -15,5 +15,11 @@ Square: 22
 */
 func main() {
 	fmt.Println("The program prints the perimeter and the square of a rectangle given the rectangle sides.")
-	// ++ Your code here! ++
+	fmt.Println("enter wight and lenght")
+	var w, l float64
+	fmt.Scanln(&w, &l)
+	p := (w + l) * 2
+	s := w * l
+	fmt.Println("S=", s)
+	fmt.Println("P=", p)
 }
