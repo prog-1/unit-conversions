950ac12dac3868f67603124c74e3c5f08d39319f
Sp0ozy <imolchanov210@gmail.com>
2021-09-26 12:58:24 +0300

    resistance done

---
+++ resistance/resistance.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("The program calculates the resistance R of two resistors.")
+	fmt.Println("enter R1 and R2 resistance")
+	var r1, r2 float64
+	fmt.Scanln(&r1, &r2)
+	R := r1 + r2
+	rr := 1/r1 + 1/r2
+	rrr := 1 / rr
+	fmt.Println("Resistance when connected in series is", R)
+	fmt.Println("Resistance when connected in parallel is", rrr)
+
+}
