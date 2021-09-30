2edfd1d25cb384418d992bf88e20d5c0829a6692
Alina Aniscenko <alina065b@gmail.com>
2021-09-30 17:13:24 +0300

    m

---
+++ maxmin/max3/max3.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("The program finds the maximum of three numbers max(a, b, c).")
+	fmt.Println("Enter three numbers:")
+	var a, b, c float64
+	fmt.Scan(&a, &b, &c)
+	max2 := (a + b + math.Abs(a-b)) / 2
+	max := (c + max2 + math.Abs(c-max2)) / 2
+	fmt.Println("Max of", a, b, c, "is", max)
+
+}
