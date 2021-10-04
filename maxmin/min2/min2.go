16407c44a93b0d7f2dfe8ac011f9257c17090e61
Денис Максимкин <you@example.com>
2021-10-04 23:01:21 +0300

    unit conversations

---
+++ maxmin/min2/min2.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("The program calculates the minimum of two numbers.")
+	fmt.Println("Enter two numers:")
+	var a, b float64
+	fmt.Scanln(&a, &b)
+	fmt.Println("min:", (a+b-math.Abs(a-b))/2)
+}
---
+++ resistance/resitance.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("The program calculates the resistance R of two resistors.")
+	fmt.Println("Enter the resistance of two resistors:")
+	var a, b float64
+	fmt.Scanln(&a, &b)
+	R := a + b
+	R2 := 1/a + 1/b
+	R3 := 1 / R2
+	fmt.Println("Resistance when connected in series is", R, "; resistance when connected in parallel is", R3)
+
+}
