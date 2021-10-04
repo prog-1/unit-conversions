b267d7332ed767947d82e15129a717bc4967511a
Marija Z <marija.zunda@gmail.com>
2021-10-04 19:32:41 +0300

    adding rectangle

--- rectangle/rectangle.go
+++ rectangle/rectangle.go
@@ -15,5 +15,9 @@ Square: 22
 */
 func main() {
 	fmt.Println("The program prints the perimeter and the square of a rectangle given the rectangle sides.")
-	// ++ Your code here! ++
+	var a, b float64
+	fmt.Println("Enter the lenght and the breadth of the rectangle")
+	fmt.Scanln(&a, &b)
+	fmt.Println("Perimeter:", (a+b)*2)
+	fmt.Println("Square:", a*b)
 }
