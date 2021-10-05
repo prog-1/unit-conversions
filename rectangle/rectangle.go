4eea2033b3115521a65b229d77cd3594901d936b
Alina <alinaleladze@gmail.com>
2021-10-05 21:36:49 +0300

    adding rectangle

--- rectangle/rectangle.go
+++ rectangle/rectangle.go
@@ -15,5 +15,11 @@ Square: 22
 */
 func main() {
 	fmt.Println("The program prints the perimeter and the square of a rectangle given the rectangle sides.")
-	// ++ Your code here! ++
+	fmt.Println("Enter the resistance of two resistors:")
+	var a,b float64
+	fmt.Scanln(&a, &b)
+	R := a + b
+	G:= 1/a +1/b
+	R3 := 1 / G
+	fmt.Ptinln("Resistance when connected in series is", R, "resistance when connected in parallel is", R3)
 }
