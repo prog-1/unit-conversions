6b503b6b8a2fe32e391cae38d86859bb946a5caf
dashk <danikputranss@gmail.com>
2021-10-08 14:08:11 +0100

    rectangle

--- rectangle/rectangle.go
+++ rectangle/rectangle.go
@@ -2,18 +2,11 @@ package main
 
 import "fmt"
 
-/*
-
-Sample input/output:
-
-The program prints the perimeter and the square of a rectangle given the rectangle sides.
-Enter the length and the breadth of the rectangle:
-4 5.5
-Perimeter: 19
-Square: 22
-
-*/
 func main() {
 	fmt.Println("The program prints the perimeter and the square of a rectangle given the rectangle sides.")
-	// ++ Your code here! ++
+	var l, b float64
+	fmt.Println("Enter the length and the breadth of the rectangle:")
+	fmt.Scanln(&l, &b)
+	fmt.Println("Perimetr:", (l+b)*2)
+	fmt.Println("Square:", l*b)
 }
