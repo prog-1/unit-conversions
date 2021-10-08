5e4b369ef210448ed31578a6a77cb2ac56bb1d32
dashk <danikputranss@gmail.com>
2021-10-08 14:46:51 +0100

    speed

--- speed/speed.go
+++ speed/speed.go
@@ -2,17 +2,10 @@ package main
 
 import "fmt"
 
-/*
-
-Sample input/output:
-
-The program converts km/h to m/s.
-Enter the speed in km/h:
-100
-The speed in m/s: 27.77777777777778
-
-*/
 func main() {
 	fmt.Println("The program converts km/h to m/s.")
-	// ++ Your code here! ++
+	var kmh float64
+	fmt.Println("Enter the speed in km/h:")
+	fmt.Scan(&kmh)
+	fmt.Println("The speed in m/s", kmh/3.6)
 }
