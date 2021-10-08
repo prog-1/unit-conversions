2686ea8a8b3080a46ab680b23dab3ae096a8aed3
dashk <danikputranss@gmail.com>
2021-10-08 14:02:33 +0100

    quadratic

---
+++ quadratic/quadratic.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("The program finds solutions of a quadratic equation.")
+	var a, b, c float64
+	fmt.Println("Enter the coefficients A, B and C:")
+	fmt.Scan(&a, &b, &c)
+	D := b*b - 4*a*c
+	fmt.Println("x1=", (-b+math.Sqrt(D))/(2*a))
+	fmt.Println("x2=", (-b-math.Sqrt(D))/(2*a))
+
+}
