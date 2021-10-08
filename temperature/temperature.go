e11ba36f87c40015fe2930ae0a8bce0563f2f4cb
dashk <danikputranss@gmail.com>
2021-10-08 14:51:19 +0100

    temperature

--- temperature/temperature.go
+++ temperature/temperature.go
@@ -2,16 +2,6 @@ package main
 
 import "fmt"
 
-/*
-
-Sample input/output:
-
-The program converts temperature from Celsius to Fahrenheit.
-Enter the temperature in Celsius:
-100
-The temperature in Fahrenheit: 212
-
-*/
 func main() {
 	fmt.Println("The program converts temperature from Celsius to Fahrenheit.")
 	var temperatureC float64
