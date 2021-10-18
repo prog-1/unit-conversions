2d5f03f120ea237ff53924fda6b34d1cfde37463
Barash456 <denis.barannik@gmail.com>
2021-10-19 00:45:05 +0300

    circle commit

--- circle/circle.go
+++ circle/circle.go
@@ -14,15 +14,13 @@ Enter the radius:
 8
 Perimeter: c
 Square: 201.06192982974676
-
 */
 func main() {
 	fmt.Println("The program prints the perimeter and the square of a circle given the radius.")
 	var radius float64
-	fmt.Println("radius:")
-	fmt.Scanln(radius)
-	fmt.Println("Peremiter:", 2*math.Pi*radius)
+	fmt.Print("radius:")
+	fmt.Scanln(&radius)
+	fmt.Println("Perimiter:", 2*math.Pi*radius)
 	fmt.Println("Square:", math.Pi*(radius*radius))
-
-	// Hint: Use math.Pi constant (https://pkg.go.dev/math#pkg-constants) to obtain π value.
+	// Hint: Use math.Pi constant (https://pkg.go.dev/math#pkg-constants) to obtain  value.π
 }
