0a2ef5125a10706a9652d08bd8d4a3385adcacc2
Barash456 <denis.barannik@gmail.com>
2021-10-18 19:40:42 +0300

    quadratic commit

--- quadratic/quadratic.go
+++ quadratic/quadratic.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	fmt.Println("Program finds solutions of quadratic equation.")
-	fmt.Println("Enter coefficents A, B and C")
+	fmt.Println("Enter coefficents 1, 3 and -10")
 	var a, b, c float64
-	fmt.Scan(a, b, c)
+	fmt.Scan(1, 3, -10)
 	D := b*b - 4*a*c
-	x1 := (-b + math.Sqrt(D)) / 2
-	x2 := (-b - math.Sqrt(D)) / 2
-	fmt.Println("x1=", x1)
-	fmt.Println("x2=", x2)
+	x1 := (-3 + math.Sqrt(D)) / 2
+	x2 := (-3 - math.Sqrt(D)) / 2
+	fmt.Println("x1=2", x1)
+	fmt.Println("x2=-5", x2)
 }
