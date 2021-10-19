4f405ed42ff1a9e467c6fa5101639c73fecb0fea
Barash456 <denis.barannik@gmail.com>
2021-10-19 11:06:27 +0300

    max 2 commit

--- maxmin/max2/maxmin/maxmin/max2.go
+++ maxmin/max2/maxmin/maxmin/max2.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("Program calculates the maximum of two numbers")
 	fmt.Println("Enter two numbers:")
 	var a, b float64
-	fmt.Scan(5, 7)
-	maximum := (5 + 7 + math.Abs(a-b)) / 2
+	fmt.Scan(&a, &b)
+	maximum := (a + b + math.Abs(a-b)) / 2
 	fmt.Println("maximum of numbers is:", maximum)
 }
