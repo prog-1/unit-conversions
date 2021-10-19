f9f7af3a51233a75376ac40b39b00062e74f1509
Barash456 <denis.barannik@gmail.com>
2021-10-19 11:06:54 +0300

    min 3 commit

--- maxmin/min3/maxmin/min3.go
+++ maxmin/min3/maxmin/min3.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 	fmt.Println("program that calculates the minimum of three numbers ")
-	fmt.Println("5, 7, 10:")
+	fmt.Println("Write three numbers:")
 	var a, b, c float64
-	fmt.Scan(5, 7, 10)
+	fmt.Scan(&a, &b, &c)
+	x := (a + b - math.Abs(a-b)) / 2
+	minimum := (x + c - math.Abs(x-c)) / 2
+	fmt.Println("Minimum:", minimum)
 
-	fmt.Println("min=", min(min(a, b), c))
 }
