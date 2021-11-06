27c0b649acb9a870ae0ceaa51ab26f57c52572bd
unknown <prosperitypeive@gmail.com>
2021-11-06 14:56:40 +0200

    fixed quadratic.go

--- quadratic/quadratic.go
+++ quadratic/quadratic.go
@@ -10,8 +10,8 @@ func main() {
 	fmt.Println("Enter the coefficients A, B and C:")
 	var A, B, C float64
 	fmt.Scan(&A, &B, &C)
-	x1 := (-B + math.Sqrt(B*B-4*A*C)) / 2 * A
-	x2 := (-B - math.Sqrt(B*B-4*A*C)) / 2 * A
+	x1 := (-B + math.Sqrt(B*B-4*A*C)) / (2 * A)
+	x2 := (-B - math.Sqrt(B*B-4*A*C)) / (2 * A)
 	fmt.Println("x1 =", x1)
 	fmt.Println("x2 =", x2)
 }
